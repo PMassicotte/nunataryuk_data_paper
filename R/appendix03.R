9b0ce3ac2c83976527142b2cf133269e756d600e
Philippe Massicotte <pmassicotte@hotmail.com>
2022-02-07 06:49:32 -0500

    Appendix 3: using air pressure to calibrate CTD depth sensor.

---
+++ R/appendix03.R
@@ -0,0 +1,60 @@
+df <- read_csv(
+  here("~", "Downloads", "Nunataryuk_depth_tare.csv"),
+  name_repair = janitor::make_clean_names
+)
+
+df
+
+df %>%
+  ggplot(aes(x = pres_dbar_ec, y = ctd_press_dbar_avg)) +
+  geom_point(aes(color = df_sector)) +
+  paletteer::scale_color_paletteer_d(
+    "suffrager::CarolMan",
+    guide = guide_legend(
+      label.theme = element_text(size = 6, family = "Roboto Light"),
+      label.position = "bottom",
+      override.aes = list(size = 2),
+      ncol = 2
+    )
+  ) +
+  scale_y_continuous(breaks = scales::breaks_pretty()) +
+  scale_x_continuous(breaks = scales::breaks_pretty()) +
+  geom_smooth(method = "lm", size = 0.5, color = "#E25247FF", alpha = 0.25) +
+  ggpmisc::stat_poly_eq(
+    aes(label = ..eq.label..),
+    label.y.npc = 0.95,
+    size = 2.5,
+    family = "Montserrat"
+  ) +
+  ggpmisc::stat_poly_eq(
+    label.y.npc = 0.90,
+    aes(label = ..rr.label..),
+    size = 2.5,
+    family = "Montserrat"
+  ) +
+  labs(
+    x = "Environment Canada atmospheric pressure (kPa)",
+    y = "In-situ CTD based depth measured in air (m)"
+  ) +
+  theme(
+    legend.justification = c(1, 0),
+    legend.position = c(0.99, 0.05),
+    legend.title = element_blank(),
+    legend.direction = "horizontal"
+  )
+
+ggsave(
+  "~/Desktop/appendix03.pdf",
+  device = cairo_pdf,
+  width = 120,
+  height = 90,
+  units = "mm"
+)
+
+ggsave(
+  "~/Desktop/appendix03.png",
+  dpi = 300,
+  width = 120,
+  height = 90,
+  units = "mm"
+)
